bc6dbad7308633678037f5ca989f527829215c8e
kimjh <kimjh.snu@gmail.com>
2018-09-25 17:52:54 +0900

    updated makefile

---
+++ client/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+py_modules = ['PyQT5']
+setup(name='client',
+    description='cn01',
+    author='kimjh12',
+    py_modules = py_modules
+    )
